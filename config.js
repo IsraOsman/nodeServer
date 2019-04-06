a01503f6b6af4763b579edc2f757a495f8741263
Isra O <Isra@israadam1993.com>
2019-04-05 23:35:39 -0500

    deleted configjs

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'SQxavkwEqidBtd4rXkK4oivDB',  
-    consumer_secret: '7b7lMAy8MgdW6i4iRlH9inpz6xCsDEyHoH9olLNQlg5yAJSqIm',
-    access_token: '1412969904-EX2IPcj94XdtvVAeUiPurko9AwFSBDyMQ2RtOkN',  
-    access_token_secret: 'mokK5R79kmJ3NiqOcw5b0BXMUlfN2wsNZGs2iOdAd1n4l'
-  }
